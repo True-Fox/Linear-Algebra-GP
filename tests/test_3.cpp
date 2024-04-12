3ab1ab4fb0a6424b0f451a93834e7a9f11979847
True-Fox <pes1202100682@pesu.pes.edu>
2024-04-13 01:04:04 +0530

    added test_3 file

---
+++ tests/test_3.cpp
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<iostream>
+
+#include "../linAlg/Matrix/matrix.h"
+#include "../linAlg/Matrix/vector.h"
+
+#include "../linAlg/Utils/Utils.h"
+
+int main(){
+    std::cout<<"Transpose (Matrix):\n";
+    Matrix<int, 2, 2> vec {1,2,3,4};
+    print(vec);
+    auto res = transpose(vec);
+    print(res);
+
+    std::cout<<"Transpose (Vector):\n";
+    Vector<int, 4> vec1 {1,2,3,4};
+    print(vec1);
+    auto res1 = transpose(vec1);
+    print(res1);
+    Matrix<int, 4,1> vec2 {1,2,3,4};
+
+    auto res3 = vec1 * vec2;
+
+    std::cout<<"Trial-1\n";
+    auto res2 = transpose(vec1) * vec1 ;
+    print(res2);
+
+}
